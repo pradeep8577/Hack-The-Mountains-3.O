e57c724b6b61e051f3997f393f53f28ea2055f4a
Pradeep Pawar <72540482+pradeep8577@users.noreply.github.com>
2022-09-18 21:38:20 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -49,47 +49,6 @@ const Home = () => {
         </p>
       </div>
       <br />
-      {/* <div className="container">
-        <p align="justify">
-          Centralised Public Grievance Redress and Monitoring System (CPGRAMS)
-          is an online platform available to the citizens 24x7 to lodge their
-          grievances to the respective authorities on any subject related to
-          service delivery. It is a single portal which can connect to all the
-          Ministries/Departments of Government of India and States , also can be
-          utilised in private sector such as business houses,collage
-          universities etc. Every Departments have role-based access to this
-          system. CPGRAMS is also accessible to the citizens through standalone
-          mobile application downloadable through Google Play store and mobile
-          application.
-        </p>
-        <br />
-        <p align="justify">
-          The status of the grievance filed in CPGRAMS can be tracked with the
-          unique registration ID provided at the time of registration of the
-          complainant. CPGRAMS also provides appeal facility to the citizens if
-          they are not satisfied with the resolution by the Grievance Officer.
-          The status of the Appeal can also be tracked by the petitioner with
-          the grievance registration number.
-        </p>
-        <h2> Issues which are not taken up for redressal</h2>
-        <hr />
-        <ul>
-          <li>
-            {" "}
-            Subjudice cases or any matter concerning judgment given by any
-            court.
-          </li>
-          <li> Personal and family disputes.</li>
-          <li> RTI matters.</li>
-          <li> Suggestions</li>
-          <li>
-            {" "}
-            Anything that impacts upon territorial integrity of the country or
-            friendly relations with other countries.
-          </li>
-        </ul>
-        <hr />
-      </div> */}
     </div>
     </>
   );
